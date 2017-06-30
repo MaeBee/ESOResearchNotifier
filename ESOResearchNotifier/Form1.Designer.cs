e7acd5ddaadc3585e39cba2fdbb8bd169908a0bf
gobbo1008 <technofreak91@web.de>
2017-06-30 11:52:52 +0200

    Initial commit
    Next step: Deserialisation

---
+++ ESOResearchNotifier/Form1.Designer.cs
@@ -0,0 +1,292 @@
+﻿namespace ESOResearchNotifier
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.notifyMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.menuItemMain = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuItemMute = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuItemExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cboCharacter = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cboAccount = new System.Windows.Forms.ComboBox();
+            this.cboNotifyStyle = new System.Windows.Forms.ComboBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.cboTimeout = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.chkMute = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            this.notifyMenuStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.Filter = "ResearchDump.lua";
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            this.fileSystemWatcher1.Changed += new System.IO.FileSystemEventHandler(this.fileSystemWatcher1_Changed);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon1.BalloonTipTitle = "ESO Research Notifier";
+            this.notifyIcon1.ContextMenuStrip = this.notifyMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "ESO Research Notifier";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // notifyMenuStrip1
+            // 
+            this.notifyMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuItemMain,
+            this.menuItemMute,
+            this.menuItemExit});
+            this.notifyMenuStrip1.Name = "notifyMenuStrip1";
+            this.notifyMenuStrip1.Size = new System.Drawing.Size(149, 70);
+            // 
+            // menuItemMain
+            // 
+            this.menuItemMain.DoubleClickEnabled = true;
+            this.menuItemMain.Name = "menuItemMain";
+            this.menuItemMain.Size = new System.Drawing.Size(148, 22);
+            this.menuItemMain.Text = "Main Window";
+            this.menuItemMain.ToolTipText = "Show the main window";
+            this.menuItemMain.Click += new System.EventHandler(this.menuItemMain_Click);
+            // 
+            // menuItemMute
+            // 
+            this.menuItemMute.CheckOnClick = true;
+            this.menuItemMute.Name = "menuItemMute";
+            this.menuItemMute.Size = new System.Drawing.Size(148, 22);
+            this.menuItemMute.Text = "Mute";
+            this.menuItemMute.ToolTipText = "Stop notifications from popping up";
+            this.menuItemMute.Click += new System.EventHandler(this.menuItemMute_Click);
+            // 
+            // menuItemExit
+            // 
+            this.menuItemExit.Name = "menuItemExit";
+            this.menuItemExit.Size = new System.Drawing.Size(148, 22);
+            this.menuItemExit.Text = "Exit";
+            this.menuItemExit.ToolTipText = "Exit Application";
+            this.menuItemExit.Click += new System.EventHandler(this.menuItemExit_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(197, 201);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 9999;
+            this.button1.Text = "Close";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cboCharacter);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.cboAccount);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(260, 73);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Character";
+            // 
+            // cboCharacter
+            // 
+            this.cboCharacter.FormattingEnabled = true;
+            this.cboCharacter.Location = new System.Drawing.Point(120, 46);
+            this.cboCharacter.Name = "cboCharacter";
+            this.cboCharacter.Size = new System.Drawing.Size(134, 21);
+            this.cboCharacter.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(6, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 23);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Character";
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(6, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Account";
+            // 
+            // cboAccount
+            // 
+            this.cboAccount.FormattingEnabled = true;
+            this.cboAccount.Location = new System.Drawing.Point(120, 19);
+            this.cboAccount.Name = "cboAccount";
+            this.cboAccount.Size = new System.Drawing.Size(134, 21);
+            this.cboAccount.TabIndex = 0;
+            // 
+            // cboNotifyStyle
+            // 
+            this.cboNotifyStyle.FormattingEnabled = true;
+            this.cboNotifyStyle.Location = new System.Drawing.Point(119, 19);
+            this.cboNotifyStyle.Name = "cboNotifyStyle";
+            this.cboNotifyStyle.Size = new System.Drawing.Size(134, 21);
+            this.cboNotifyStyle.TabIndex = 2;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.cboTimeout);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.chkMute);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.cboNotifyStyle);
+            this.groupBox2.Location = new System.Drawing.Point(13, 92);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(259, 103);
+            this.groupBox2.TabIndex = 10000;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Notification Appearance";
+            // 
+            // cboTimeout
+            // 
+            this.cboTimeout.FormattingEnabled = true;
+            this.cboTimeout.Location = new System.Drawing.Point(119, 46);
+            this.cboTimeout.Name = "cboTimeout";
+            this.cboTimeout.Size = new System.Drawing.Size(134, 21);
+            this.cboTimeout.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(6, 49);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(108, 23);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Notification Timeout";
+            // 
+            // chkMute
+            // 
+            this.chkMute.Location = new System.Drawing.Point(119, 73);
+            this.chkMute.Name = "chkMute";
+            this.chkMute.Size = new System.Drawing.Size(104, 24);
+            this.chkMute.TabIndex = 4;
+            this.chkMute.Text = "Mute";
+            this.chkMute.UseVisualStyleBackColor = true;
+            this.chkMute.CheckedChanged += new System.EventHandler(this.chkMute_CheckedChanged);
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(5, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 23);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Notification Style";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(140, 201);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(51, 23);
+            this.button2.TabIndex = 10001;
+            this.button2.Text = "X";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.Location = new System.Drawing.Point(18, 206);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(124, 23);
+            this.linkLabel1.TabIndex = 5;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "ResearchDump AddOn";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 236);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "ESO Research Notifier";
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            this.notifyMenuStrip1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip notifyMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuItemMain;
+        private System.Windows.Forms.ToolStripMenuItem menuItemMute;
+        private System.Windows.Forms.ToolStripMenuItem menuItemExit;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.CheckBox chkMute;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cboNotifyStyle;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox cboCharacter;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cboAccount;
+        private System.Windows.Forms.ComboBox cboTimeout;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ ESOResearchNotifier/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+using ToastNotifications;
+
+namespace ESOResearchNotifier
+{
+    public partial class Form1 : Form
+    {
+        private enum NotificationType { Balloon, Toast };
+        private enum CraftingType { Stable = 0, Blacksmithing = 1, Clothier = 2, Woodworking = 6};
+        private int[] BlacksmithingTypes = { 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1 };
+        private int[] ClothierTypes = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+        private int[] WoodworkingTypes = { 0, 0, 0, 0, 0, 1 };
+        private string[] WeaponTraits = { "Powered", "Charged", "Precise", "Infused", "Defending", "Training", "Sharpened", "Decisive", "Nirnhoned" };
+        private string[] ArmourTraits = { "Sturdy", "Impenetrable", "Reinforced", "Well-Fitted", "Training", "Infused", "Prosperous", "Divines", "Nirnhoned" };
+        private bool Mute = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            cboTimeout.Items.Add(new ComboboxItem("1 second", 1000));
+            cboTimeout.Items.Add(new ComboboxItem("3 seconds", 3000));
+            cboTimeout.Items.Add(new ComboboxItem("5 seconds", 5000));
+            cboTimeout.Items.Add(new ComboboxItem("8 seconds", 8000));
+            cboTimeout.Items.Add(new ComboboxItem("10 seconds", 10000));
+            cboTimeout.SelectedIndex = 2;
+
+            cboNotifyStyle.Items.Add(new ComboboxItem("Balloon", NotificationType.Balloon));
+            cboNotifyStyle.Items.Add(new ComboboxItem("Toast", NotificationType.Toast));
+            cboNotifyStyle.SelectedIndex = 0;
+
+            fileSystemWatcher1.Path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "Elder Scrolls Online\\live\\SavedVariables");
+
+            ReadDump();
+        }
+
+        private void ShowNotification(string Content)
+        {
+            if (!Mute)
+            {
+                ComboboxItem NotifyStyle = (ComboboxItem)cboNotifyStyle.SelectedItem;
+                ComboboxItem Timeout = (ComboboxItem)cboTimeout.SelectedItem;
+
+                if ((NotificationType)NotifyStyle.Value == NotificationType.Balloon)
+                {
+                    notifyIcon1.BalloonTipText = Content;
+                    notifyIcon1.ShowBalloonTip((int)Timeout.Value);
+                }
+                else if ((NotificationType)NotifyStyle.Value == NotificationType.Toast)
+                {
+                    Notification toastNotification = new Notification("ESO Research Notifier", Content, (int)Timeout.Value, FormAnimator.AnimationMethod.Slide, FormAnimator.AnimationDirection.Up);
+                    toastNotification.Show();
+                }
+            }
+        }
+
+        private void ReadDump()
+        {
+            if (!File.Exists(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "Elder Scrolls Online\\live\\SavedVariables\\ResearchDump.lua")))
+            {
+                MessageBox.Show("Please run the ResearchDump addon first and /reloadui to generate utilisable data");
+                return;
+            }
+        }
+
+        private void fileSystemWatcher1_Changed(object sender, System.IO.FileSystemEventArgs e)
+        {
+            ReadDump();
+        }
+
+        private void menuItemMain_Click(object sender, EventArgs e)
+        {
+            Show();
+            this.WindowState = FormWindowState.Normal;
+        }
+
+        private void menuItemMute_Click(object sender, EventArgs e)
+        {
+            Mute = chkMute.Checked = menuItemMute.Checked;
+        }
+
+        private void chkMute_CheckedChanged(object sender, EventArgs e)
+        {
+            Mute = menuItemMute.Checked = chkMute.Checked;
+        }
+
+        private void menuItemExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                Hide();
+                this.ShowInTaskbar = false;
+            }
+            else
+            {
+                this.ShowInTaskbar = true;
+            }
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Show();
+            this.WindowState = FormWindowState.Normal;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ShowNotification("This is a test.");
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/gobbo1008/ResearchDump/releases/latest");
+        }
+    }
+
+    public class ComboboxItem
+    {
+        public string Text { get; set; }
+        public object Value { get; set; }
+
+        public ComboboxItem() : this(null, null)
+        {
+        }
+
+        public ComboboxItem(string Text, object Value)
+        {
+            this.Text = Text;
+            this.Value = Value;
+        }
+
+        public override string ToString()
+        {
+            return Text;
+        }
+    }
+}
